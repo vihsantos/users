e98151e03c38c3baf65172bee99559db04dfd38c
Vitória <vick_santos_vick@hotmail.com>
2023-06-01 21:55:42 -0300

    --- criação da controller

---
+++ Users/Controller/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Users.Model;
+
+namespace Users.Controller
+{
+    public class UsuarioController
+    {
+
+    }
+}
